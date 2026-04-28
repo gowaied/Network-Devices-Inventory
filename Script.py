4348a91957b8fc08bd3bbcaa0fd51e34b8ccb5e0
Mohamed Gowaied <168038049+gowaied@users.noreply.github.com>
2026-04-28 23:47:52 +0300

    Script.py

--- Script.py
+++ Script.py
@@ -1,126 +1,51 @@
 from netmiko import ConnectHandler, NetMikoAuthenticationException, NetMikoTimeoutException
 from getpass import getpass
-import ipaddress
-import time
+import os
 
-switches = {
-
+all_devices = {
     'SW1' : '192.168.81.140',
-    'SW2' : '192.168.81.141'
-}
-
-routers = {
-
+    'SW2' : '192.168.81.141',
     'R1' : '192.168.81.139',
     'R2' : '192.168.81.134',
-    'R3' : '192.168.81.137',
-    'R4' : '192.168.81.138',
+    'R3' : ' 192.168.81.137',
+    'R4' : '192.168.81.138'
 }
 
-ospf_networks = {
-    'R1' : ['192.168.81.139', '172.16.0.1', '10.0.0.1', '1.1.1.1'],
-    'R2' : ['192.168.81.134', '172.16.0.2', '10.0.0.2', '2.2.2.2'],
-    'R3' : ['192.168.81.137', '172.16.0.3', '10.0.0.3', '3.3.3.3'],
-    'R4' : ['192.168.81.138', '172.16.0.4', '10.0.0.4', '4.4.4.4'],
-}
+password = getpass('Enter Password": ')
+secret = getpass('Enter Secret": ')
 
-password = getpass('Enter Password: ')
-secret = getpass('Enter Secret: ')
-
-for switch, ip in switches.items():
-
-    device = {
-        'device_type' : 'cisco_ios',
-        'host' : ip,
-        'username' : 'admin',
-        'password' : password,
-        'secret' : secret
-    }
-
+for host, ip in all_devices.items():
     try:
-        print(f'Connecting to {switch}..........')
-        time.sleep(1)
-        ssh_switch = ConnectHandler(**device)
-        ssh_switch.enable()
+        device = {
+            'device_type' : 'cisco_ios',
+            'host' : ip,
+            'username' : 'admin',
+            'password' : password,
+            'secret' : secret
+        }
 
-        while True:
-            loopback_ip = input('Loopback IP: ')
-            loopback_mask = input('Loopback Subnet Mask: ')
+        print(f'Connecting to {host}..........')
 
-            try:
-                ipaddress.IPv4Network(loopback_ip)
-                break
-            except ipaddress.AddressValueError:
-                print(f'{loopback_ip} is not valid, please try another one!')
-        
-        commands = [
-            'user gowaied priv 15 secret Mohamed',
-            'int lo 0',
-            f'ip address {loopback_ip} {loopback_mask}'
-        ]
-        config = ssh_switch.send_config_set(commands)
-        print(config)
-        verification = ssh_switch.send_command('sh ip int br | ex un')
-        print(verification)
-        ssh_switch.save_config()
+        ssh = ConnectHandler(**device)
+        print(f'Gathering {host}\'s data.........')
+    
+        show_ip = ssh.send_command('show ip int br | ex un')
+        show_version = ssh.send_command('show version')
+        start_config = ssh.send_command('show start')
+
+        os.makedirs(rf'E:\Cisco Devices Inventory\{host}', exist_ok=True)
+        device_interfaces = open(rf'E:\Cisco Devices Inventory\{host}\Active_Interface.txt','x')
+        device_interfaces.write(show_ip)
+        device_version = open(rf'E:\Cisco Devices Inventory\{host}\Version.txt','x')
+        device_version.write(show_version)
+        device_config = open(rf'E:\Cisco Devices Inventory\{host}\config.txt','x')
+        device_config.write(start_config)
+
+        print(f'{host} has been added to the inventory!')
+    
 
     except NetMikoAuthenticationException:
-        print(f'Failed to connect to {switch}, please check credintials!')
+        print(f'Failed to connect to {host}, please check credintials!')
+
     except NetMikoTimeoutException:
-        print(f'Failed to connect to {switch}, please check reachability!')
-
-
-for router, ip in routers.items():
-
-    device = {
-        'device_type' : 'cisco_ios',
-        'host' : ip,
-        'username' : 'admin',
-        'password' : password,
-        'secret' : secret
-    }
-
-    try:
-        print('**************************************************************************')
-        print(f'Connecting to {router}..........')
-        time.sleep(1)
-        ssh_router = ConnectHandler(**device)
-        ssh_router.enable()
-
-        while True:
-            loopback_ip = input('Loopback IP: ')
-            loopback_mask = input('Loopback Subnet Mask: ')
-            try:
-                ipaddress.IPv4Network(loopback_ip)
-                break
-            except ipaddress.AddressValueError:
-                print(f'{loopback_ip} is invalid, please try another one!')
-
-        commands = [
-            'user gowaied priv 15 secret Mohamed',
-            'int lo 0',
-            f'ip address {loopback_ip} {loopback_mask}',
-            'router ospf 1',
-            'passive-interface lo0'
-        ]
-
-        if router in ospf_networks:
-            networks = ospf_networks[router]
-            for network in networks:
-                commands.append(f'network {network} 0.0.0.0 area 0')
-
-        config = ssh_router.send_config_set(commands)
-        print(config)
-        loopback_verification = ssh_router.send_command('sh ip int br | ex un')
-        print(loopback_verification)
-        print('**************************************************************************')
-        ospf_verification = ssh_router.send_command('sh ip proto | sec ospf')
-        print(ospf_verification)
-
-        ssh_router.save_config()
-    except NetMikoAuthenticationException:
-        print(f'Failed to connect to {router}, please check credintials!')
-    except NetMikoTimeoutException:
-        print(f'Failed to connect to {router}, please check reachability!')
-
-
+        print(f'Failed to connect to {host}, please check reachability!')
